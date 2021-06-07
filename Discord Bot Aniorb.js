d5b959be95abf743fe911d3c305094e300a0a659
Aris A <82681339+Chibi-Usa@users.noreply.github.com>
2021-06-07 13:38:51 -0700

    basically the magic stuffs
    This is basically what is the main part of the bot
    
    and i hope to god it works
    
    cause i have no fucking idea what i am doing and i think i am mostly half assing this

---
+++ Discord Bot Aniorb.js
@@ -0,0 +1,9 @@
+/*(oh god i have no fucking idea what im doing but here we fucking go)*/
+
+const { DiscordAPIError } = require("discord.js");
+
+/*(this bot was created by Arisu#0404 discord u/lolsforalls23 reddit)*/
+
+/*(currently it does nothing right now but that will change, i hope)*/
+
+Discord.js 
