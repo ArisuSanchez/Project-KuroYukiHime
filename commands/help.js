17d7aa5921fd86509bbe7a080ba7f23304231d90
Chibi Usa <redkahoot@gmail.com>
2021-06-08 20:15:14 -0700

    added help command
    dk if it works tho
    
    probs not as im copy pasting bs
    
    :lol:

---
+++ commands/help.js
@@ -0,0 +1,8 @@
+const {MessageEmbed} = require('discord.js')
+exports.execute = async (client, message, args) => {
+    message.channel.send(`Please visit https://www.arisubot.me/bot-fq for a list of all the current bot commands`)
+};
+exports.help = {
+    name: "help",
+    aliases: ['h']
+  };
