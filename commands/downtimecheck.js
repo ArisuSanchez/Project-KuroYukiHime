7abee058e4c0a94121cb3fb57df554c16ec763ae
Chibi Usa <redkahoot@gmail.com>
2021-06-09 14:13:30 -0700

    added a better downtime command

---
+++ commands/downtimecheck.js
@@ -0,0 +1,8 @@
+exports.execute = async (client, message, args) => {
+    message.channel.reply(`Here Is The link to check for downtime https://www.arisubot.me/bot-status`)
+};
+exports.help = {
+    name: "downtime",
+    aliases: ['d']
+  };
+  //am i doing this right?-a
