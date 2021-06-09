fecf9ce50d500abf2e217550bab169bef33976c1
Chibi Usa <redkahoot@gmail.com>
2021-06-08 18:38:53 -0700

    okay good update this time lol hope
    added manga searching
    
    coming soon
    
    rss feeds

--- commands/searchmanga.js
+++ commands/searchmanga.js
@@ -1,10 +1,10 @@
 const { MessageEmbed } = require('discord.js')
 exports.execute = async (client, message, args) => {
-    let baseurl = 'https://aniorb.me/search/'
+    let baseurl = 'https://comick.fun/search?q='
     const dentifier = args.slice().join('%20')
     const embed = new MessageEmbed()
-    const url = `${baseurl}` + `${dentifier}` + `/1`;
-    embed.setTitle(`🔎 Search Results For Anime ${args.slice().join(' ')}`)
+    const url = `${baseurl}` + `${dentifier}`;
+    embed.setTitle(`🔎 Search Results For Manga ${args.slice().join(' ')}`)
     embed.setColor('RANDOM')
     embed.setDescription(`**[Here Is What You Wanted](${url})**`)
     embed.setTimestamp()
@@ -12,9 +12,9 @@ exports.execute = async (client, message, args) => {
     message.channel.send(`Please Check Your Direct Messages For The Search Results!`)
     message.author.send(embed)
   
-};
+}
 
 exports.help = {
-  name: "search",
-  aliases: ['s']
+  name: "searchmanga",
+  aliases: ['sm']
 };
--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    token: "ODUxNTU4NTAwNzU1OTYzOTY2.YL6Bpw.Psr3cGQX3XKHjdUKeHPCz8nxpGQ",
+    token: "",
     prefix: "!",
 }
