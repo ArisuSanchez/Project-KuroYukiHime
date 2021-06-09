d9748ec2cb5653b4a5770f02106f925abda5c76d
Chibi Usa <redkahoot@gmail.com>
2021-06-08 19:03:50 -0700

    deleted a joke command
    shame

--- commands/based.js
+++
@@ -1,8 +0,0 @@
-exports.execute = async (client, message, args) => {
-    message.reply('based, based on what <:squint:834828052092682270>');
-};
-
-exports.help = {
-  name: "based",
-  aliases: []
-};
