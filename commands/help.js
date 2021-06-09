bfff90df5dfed2c9a11ff054cd7554d6d0221e9a
Chibi Usa <redkahoot@gmail.com>
2021-06-09 15:35:03 -0700

    added a thumbnail

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,5 @@
 exports.execute = async (client, message, args) => {
-    message.channel.send(`Please visit https://www.arisubot.me/bot-fq for a list of all the current bot commands`)
+    message.channel.send(`Please visit https://www.arisubot.me/bot-faq for a list of all the current bot commands`)
 };
 exports.help = {
     name: "help",
