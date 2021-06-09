67404ba157f1e040b522289896e037be0fced9d5
ZeroDiscord <68364573+ZeroDiscord@users.noreply.github.com>
2021-06-09 13:29:32 +0530

    Delete Old Downtime Command 😎

--- commands/update.js
+++
@@ -1,8 +0,0 @@
-const {Message} = require('discord.js')
-exports.execute = async (client, message, args) => {
-    message.channel.send(`There will be downtime at 9:00 Pacific Standard Time on 6/8/2021 `)
-};
-exports.help = {
-    name: "downtime",
-    aliases: ['d']
-  };
