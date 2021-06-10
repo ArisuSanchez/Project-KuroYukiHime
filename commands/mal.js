e09ffa95805fe2c9d61a096b5fec1d4ff988bf8e
Aantriksh Sood <aantrikshsood@gmail.com>
2021-06-10 13:33:34 +0530

    ♥ Added some stuffs, switched sm command to mal, added mal command, animes now have thumbnails and everything is sexy ♥

--- commands/mal.js
+++ commands/mal.js
@@ -1,17 +1,51 @@
 const { MessageEmbed } = require('discord.js')
+const Scraper = require('mal-scraper')
 exports.execute = async (client, message, args) => {
-    let baseurl = 'https://myanimelist.net/search/all?q='
-    const dentifier = args.slice().join('%20')
-    const embed = new MessageEmbed()
-    const url = `${baseurl}` + `${dentifier}` + `&cat=all`;
-    embed.setTitle(`🔎 Search Results For My Anime List, ${args.slice().join(' ')}`)
-    embed.setColor('RANDOM')
-    embed.setDescription(`**[Click Here To Open Your My Anime List Search Results](${url})**`)
-    embed.setTimestamp()
-    embed.setThumbnail('https://onii-chan.life-is-pa.in/7YFbVi.png')
-    embed.setFooter(`Made by 0_0#6666 and Arisu#0404`)
-    message.channel.send(embed)
   
+  let query = args.join(" ");
+
+  if (!query) return message.channel.send(`Please Give A Search Argument!`);
+
+  if (query.length > 150) return message.channel.send(`Query Lengths Are Limited To 150 Chars`);
+
+  let search = await message.channel.send(`**Searching for the requested anime 🔎**`);
+
+  let dentifier = query.replace(/ /g, " ");
+
+  await search.delete();
+
+  let animedata;
+
+  let Embed;
+
+  try {
+
+  animedata = await Scraper.getInfoFromName(dentifier);
+
+  if (!animedata.genres[0] || animedata.genres[0] === null) animedata.genres[0] = "None";
+  Embed = new MessageEmbed()
+  .setColor('RANDOM')
+  .setURL(animedata.url)
+  .setTitle(animedata.title)
+  .setDescription(animedata.synopsis)
+  .addField(`Type`, animedata.type, true)
+  .addField(`Status`, animedata.status, true)
+  .addField(`Started Airing`, animedata.premiered, true)
+  .addField(`Episodes`, animedata.episodes, true)
+  .addField(`Duration`, animedata.duration, true)
+  .addField(`Popularity`, animedata.popularity, true)
+  .addField(`Genres`, animedata.genres.join(", "))
+  .setThumbnail(animedata.picture)
+  .setFooter(`Rating - ${animedata.score}/10 | Made by 0_0#6666 and Arisu#0404`)
+  .setTimestamp();
+
+  } catch (error) {
+    console.log(error)
+    return message.channel.send(`No Such Anime Found!`)
+   
+  };
+
+  return message.channel.send(Embed);
 };
 
 exports.help = {
--- commands/searchmanga.js
+++ commands/searchmanga.js
@@ -1,19 +1,43 @@
-const { MessageEmbed } = require('discord.js')
+const { MessageEmbed, DiscordAPIError } = require('discord.js')
+const malScraper = require('mal-scraper')
+const search = malScraper.search
+
 exports.execute = async (client, message, args) => {
-    let baseurl = 'https://comick.fun/search?q='
-    const dentifier = args.slice().join('%20')
-    const embed = new MessageEmbed()
-    const url = `${baseurl}` + `${dentifier}`;
-    embed.setTitle(`🔎 Search Results For Manga, ${args.slice().join(' ')}`)
-    embed.setColor('RANDOM')
-    embed.setDescription(`**[Click Here To View Your Manga Search Results](${url})**`)
-    embed.setTimestamp()
-    embed.setFooter(`Made by 0_0#6666 and Arisu#0404`)
-    embed.setThumbnail('https://onii-chan.life-is-pa.in/7YFbVi.png')
-    message.channel.send(`Please Check Your Direct Messages For The Search Results!📫`)
-    message.author.send(embed)
-  
-}
+  if(!args) {
+    return message.channel.send(`Please provide a search query~~!`)
+  }
+  const type = 'manga'
+  let baseurl = 'https://comick.fun/search?q='
+  const dentifier = args.slice().join('%20')
+  const url = `${baseurl}` + `${dentifier}`;
+  const msg = await message.channel.send(`**🔍 Searching for your manga**`)
+search.search(type, {
+  maxResults: 1, // how many results at most
+  term: args.join(' '), // search term
+})
+  .then( async mangadata => {
+    let manga = new MessageEmbed()
+    const desc = mangadata[0].shortDescription.replace("...read more",`...\n**[Read More](${mangadata[0].url})**`)
+    manga.setTitle(mangadata[0].title)
+	  manga.setDescription(`${desc}\n\n **[Read This Manga For FREE over at our partner Comick™](${url})**`)
+	  manga.setThumbnail(mangadata[0].thumbnail)
+	  manga.setURL(mangadata[0].url)
+	  manga.setColor("RANDOM")
+
+	manga.addFields(
+		{ name: "Rating", value: `${mangadata[0].score}/10`, inline: true },
+		{ name: "Publishing Started", value: mangadata[0].startDate, inline: true },
+		{ name: "Published upto", value: mangadata[0].endDate, inline: true },
+		{ name: "Volume Count", value: mangadata[0].vols, inline: true },
+		{ name: "Chapter Count", value: mangadata[0].nbChapters, inline: true },
+		{ name: "Type", value: mangadata[0].type, inline: true }
+	);
+  manga.setTimestamp()
+  manga.setFooter(`Made by 0_0#6666 and Arisu#0404`)
+  await msg.edit(`**🔎 Here are the results we found!**`,manga)
+  })
+  .catch(console.error)
+    }
 
 exports.help = {
   name: "searchmanga",
