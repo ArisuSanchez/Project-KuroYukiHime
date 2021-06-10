8995317f3c4fa32c312b7daa3fbbcc06abdba037
Chibi Usa <redkahoot@gmail.com>
2021-06-09 22:41:04 -0700

    Create invite.js |Init Commit
    yuppers invite command

---
+++ commands/invite.js
@@ -0,0 +1,7 @@
+exports.execute = async (client, message, args) => {
+    message.channel.send(`To invite the bot go to https://www.arisubot.me/bot-terms-of-service and at the bottom of the page click *Invite Bot?*`)
+};
+exports.help = {
+    name: "invite",
+    aliases: ['i']
+  };
