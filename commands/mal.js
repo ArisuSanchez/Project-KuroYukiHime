28900fe701e30bbc92d41be5b81d0cc4acc6bd33
Aantriksh Sood <aantrikshsood@gmail.com>
2021-06-10 13:36:57 +0530

    ♥ Added some stuffs, switched sm command to mal, added mal command, animes now have thumbnails and everything is sexy ♥

--- commands/mal.js
+++ commands/mal.js
@@ -1,27 +1,16 @@
 const { MessageEmbed } = require('discord.js')
 const Scraper = require('mal-scraper')
 exports.execute = async (client, message, args) => {
-  
   let query = args.join(" ");
-
   if (!query) return message.channel.send(`Please Give A Search Argument!`);
-
   if (query.length > 150) return message.channel.send(`Query Lengths Are Limited To 150 Chars`);
-
   let search = await message.channel.send(`**Searching for the requested anime 🔎**`);
-
   let dentifier = query.replace(/ /g, " ");
-
   await search.delete();
-
   let animedata;
-
   let Embed;
-
   try {
-
   animedata = await Scraper.getInfoFromName(dentifier);
-
   if (!animedata.genres[0] || animedata.genres[0] === null) animedata.genres[0] = "None";
   Embed = new MessageEmbed()
   .setColor('RANDOM')
@@ -38,13 +27,10 @@ exports.execute = async (client, message, args) => {
   .setThumbnail(animedata.picture)
   .setFooter(`Rating - ${animedata.score}/10 | Made by 0_0#6666 and Arisu#0404`)
   .setTimestamp();
-
   } catch (error) {
     console.log(error)
     return message.channel.send(`No Such Anime Found!`)
-   
   };
-
   return message.channel.send(Embed);
 };
 
